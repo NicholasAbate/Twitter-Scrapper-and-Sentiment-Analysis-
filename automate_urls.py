8db4e54872867426a82e7d3f1a2fb4089212b13c
NicholasAbate <119979833+NicholasAbate@users.noreply.github.com>
2022-12-06 18:54:13 -0500

    Delete automate_urls.py

--- automate_urls.py
+++
@@ -1,9 +0,0 @@
-with open('mid_handles.txt', 'r') as f:
-    string = f.read()
-    string.strip()
-list = string.split('\n')
-list.remove('')
-urls = ['https://twitter.com']*len(list)
-for i in range(len(list)):
-    urls[i] += '/'+ list[i] + '/followers'
-    print(urls[i])
